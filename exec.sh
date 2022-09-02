ae916dc04245552f69bacfbc6c1f53397c18d284
Zhang <zhang@i22-yzhang.local>
2022-09-03 02:30:38 +0900

    debug

--- exec.sh
+++ exec.sh
@@ -3,14 +3,17 @@
 APPDIR=`dirname $0`
 cd ./repo
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
-bash Mambaforge-Linux-x86_64.sh -y
-conda init
+bash Mambaforge-Linux-x86_64.sh <<EOF
+Yes
+EOF
 source ~/.bashrc
 mamba env create -f environment.yml
 mamba activate torchmd-net
-source ~/.bashrc
 pip install -e .
-source ~/.bashrc
-CUDA_VISIBLE_DEVICES=1 python scripts/train.py --conf examples/ET-QM9.yaml $@
+# mkdir logs
+# cp examples/ET-QM9.yaml .
+# mv ET-QM9.yaml input.yaml
+# mv input.yaml logs/
+CUDA_VISIBLE_DEVICES=0 python scripts/train.py --conf examples/ET-QM9.yaml $@
 return $?
 
