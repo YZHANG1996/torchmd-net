1e0f96e90ca1b3b7cf33524f2d00bee2ed136487
Zhang <zhang@i22-yzhang.local>
2022-09-03 01:41:09 +0900

    debug

--- exec.sh
+++ exec.sh
@@ -6,11 +6,15 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforg
 bash Mambaforge-Linux-x86_64.sh <<EOF
 Yes
 Yes
+Yes
 EOF
+conda init
+source ~/.bashrc
 mamba env create -f environment.yml
 mamba activate torchmd-net
 source ~/.bashrc
 pip install -e .
+source ~/.bashrc
 CUDA_VISIBLE_DEVICES=1 python scripts/train.py --conf examples/ET-QM9.yaml $@
 return $?
 
